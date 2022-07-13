ef85e50fd2cae3891056d83664fc6c32d7f60c33
Ulisses Silva <ulissesrdasilva@gmail.com>
2022-07-13 09:52:54 -0300

    fix: type

---
+++ src/services/hashbox.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import BoxModel from '../models/Box.model';
+import * as Parse from 'parse/node';
+import { response } from 'express';
+import { BoxService } from './box.service';
+
+@Injectable()
+export class HashBoxService {
+  constructor(
+    private configService: ConfigService,
+    private boxService: BoxService,
+    private secretKey: string | undefined,
+  ) {
+    this.ParseServerConfiguration();
+    this.secretKey = this.configService.get<string>('PRIVATE_KEY');
+  }
+
+  async generateHash(toyo): Promise<any> {}
+
+  async dencryptHash(hashbox: string): Promise<any> {}
+
+  async generateSignature(player): Promise<any> {}
+
+  /**
+   * Function to configure ParseSDK
+   */
+  private ParseServerConfiguration(): void {
+    Parse.initialize(
+      this.configService.get<string>('BACK4APP_APPLICATION_ID'),
+      this.configService.get<string>('BACK4APP_JAVASCRIPT_KEY'),
+      this.configService.get<string>('BACK4APP_MASTER_KEY'),
+    );
+    (Parse as any).serverURL = this.configService.get<string>(
+      'BACK4APP_SERVER_URL',
+    );
+  }
+}
