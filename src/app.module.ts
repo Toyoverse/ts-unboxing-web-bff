ddcbbe461d7cba766a95584d67a224d3ab2ef8f0
Victor Santos Batista <victorsantos.developer@gmail.com>
2022-07-14 14:21:13 -0300

    Encrypt and decrypt toyo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { AppService } from './services/app.service';
 import { OnchainService } from './services/onchain.service';
 import { BoxService } from './services/box.service';
 import { AuthMiddleware } from './middlewares/auth.middleware';
+import { AESCrypt } from './utils/crypt/aes-crypt';
+import { HashBoxService } from './services/hashbox.service';
+import di from './di';
 
 @Module({
   imports: [
@@ -17,6 +20,8 @@ import { AuthMiddleware } from './middlewares/auth.middleware';
     AppService,
     OnchainService,
     BoxService,
+    HashBoxService,
+    { provide: di.AESCrypt, useClass: AESCrypt },
   ],
 })
 export class AppModule implements NestModule {
