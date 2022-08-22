2d7d35ed86621020eb65e65ce4a15ad934e02e8f
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2022-08-22 12:50:35 -0300

    Created background job

---
+++ src/jobs/toyoJob-consumer.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@nestjs/common";
+import { BoxService, OnchainService, ToyoService } from "src/services";
+
+@Injectable()
+export class ToyoJobConsumer{
+    constructor(
+        private readonly onChainService: OnchainService,
+        private readonly toyoService: ToyoService,
+        private readonly boxService: BoxService,
+        ){}
+
+    async saveToyoJob(result: Parse.Object<Parse.Attributes>, toyo:  Parse.Object<Parse.Attributes>[]){
+        let i:number = 0;
+        setTimeout(async()=>{
+            while (i < 5){
+                const swappedEntities =
+                await this.onChainService.getTokenSwappedEntitiesByClosedBoxTokenId(
+                    result.get('tokenIdClosedBox')
+                );
+    
+                if (swappedEntities.length === 0) {
+                    i++;
+                    
+                }else {
+                    const blockchainToyo = swappedEntities.find((i) => i.toTypeId === '9');
+                    const blockchainBox = swappedEntities.find((i) => i.toTypeId !== '9');
+    
+                    toyo[0].set('tokenId', blockchainToyo.toTokenId);
+                    toyo[0].set('transactionHash', blockchainToyo.transactionHash);
+                    await toyo[0].save();
+    
+                    await this.boxService.updateBoxFields(result, toyo, blockchainBox);
+                    i = 5;
+                }
+            }
+        }, 10000);
+    
+    }
+
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -4,3 +4,5 @@ export * from './onchain.service';
 export * from './hashbox.service';
 export * from './app.service';
 export * from './toyo.service';
+export * from '../jobs/toyoJob-consumer';
+export * from '../jobs/toyoJob-producer';
